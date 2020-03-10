5eac8f61e5139530b094d71fc89e06feaa658844
Shvedov Vladimir <shvedovvladimir27@yandex.ru>
2020-03-10 13:03:46 +0300

    fix  ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 module.exports = {
     "type": "postgres",
-    "host": process.env.POSTGRESSQL_HOST,
+    "host": process.env.POSTGRESQL_HOST,
     "port": 5432,
-    "username": process.env.POSTGRESSQL_USER,
-    "password": process.env.POSTGRESSQL_PASSWORD,
-    "database": process.env.POSTGRESSQL_DB,
-    "schema": process.env.POSTGRESSQL_SCHEMA,
+    "username": process.env.POSTGRESQL_USER,
+    "password": process.env.POSTGRESQL_PASSWORD,
+    "database": process.env.POSTGRESQL_DB,
+    "schema": process.env.POSTGRESQL_SCHEMA,
     "entities": ["src/**/**.entity.js"],
     "synchronize": false
 };
